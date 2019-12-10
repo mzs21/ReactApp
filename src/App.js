5a2611365bc9cbfa292c6f58a7f1a25df303ad81
mzs21 <snigdho2111@gmail.com>
2019-12-10 20:12:36 +0600

    GCI-Snigdho Changed the HTML title to 'GCI-2019-REACT-APP-Snigdho', changed the default image with the downloaded GCI logo, edited the 'Welcome' message, with 'Welcome to Google Code-in 2019'.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './assets/GCILogo.png';
+import newlogo from './assets/GCILogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={newlogo} className="App-logo" alt="logo" />
         <p>
         Welcome to Google Code-in 2019
         </p>
